42a86c601d718245f598cbbfe4b462c26ec33b49
Andrey Mishchenko <buryonok@gmail.com>
2022-09-01 22:17:00 +0300

    Add console.log

--- public/js/index.js
+++ public/js/index.js
@@ -163,6 +163,7 @@ document.getElementById("roll-button").addEventListener("click", () => {
     });
   } else {
     const pShouldThrow = document.createElement('p');
+    console.log(state.fieldValues, state.currentPlayer.pos);
     pShouldThrow.textContent = `You should throw <= then field value (${state.fieldValues[state.currentPlayer.pos]})`;
     document.getElementById("current-player").prepend(pShouldThrow);
     state.isStepCompleted = true;
