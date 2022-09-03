5a4aeb86307dfaac0baa523e6e05bf8cd80e70be
Andrey Mishchenko <buryonok@gmail.com>
2022-09-03 19:18:12 +0300

    Move Player to Player.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+import Player from './Player.js';
+
 // Making Connection
 const socket = io.connect();
 socket.emit("joined");
@@ -37,36 +39,6 @@ const pNextPlayer = document.createElement('p');
 pNextPlayer.textContent = 'Anyone can roll';
 const pChooseField = document.createElement('p');
 pChooseField.textContent = 'Choose one of the bright fields';
-
-class Player {
-  constructor(id, name, pos, img) {
-    this.id = id;
-    this.name = name;
-    this.pos = pos;
-    this.img = img;
-  }
-
-  draw() {
-    const image = new Image();
-    image.src = this.img;
-    image.width = 30;
-    image.height = 40;
-    image.setAttribute('alt', `Player ${this.id}`);
-    image.classList.add('pin');
-
-    const xPosTable = Math.floor(this.pos / 10) % 2 === 0
-      ? (this.pos % 10 + 1)
-      : 10 - ((this.pos % 10));
-
-    const yPosTable = 10 - (Math.floor(this.pos / 10));
-    const currentField = document.querySelector(`.board tr:nth-child(${yPosTable}) td:nth-child(${xPosTable})`);
-    currentField.append(image);
-  }
-
-  updatePos(num) {
-    this.pos = num;
-  }
-}
 
 const drawBoard = (values) => {
   fields.forEach((field, i) => {
